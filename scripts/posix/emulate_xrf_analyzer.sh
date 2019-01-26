b931cc2d97ada5069c0b18bdc1dc55e1f75ce723
Stephen Gowen <dev.sgowen@gmail.com>
2019-01-26 02:28:15 -0500

    Order matters, also, including links for convenience

--- scripts/posix/emulate_xrf_analyzer.sh
+++ scripts/posix/emulate_xrf_analyzer.sh
@@ -16,16 +16,28 @@ work_dir=$(mktemp -d)
 
 pushd $work_dir
 
-echo 'Pulling Emulator Support Files...'
+echo 'Pulling Apps...'
 XRFAndroidApkFileName=$(adb -d shell ls /data/app/com.sciaps.xrf* | xargs -n 1 basename)
 XRFHomeApkFileName=$(adb -d shell ls /data/app/com.sciaps.android.home* | xargs -n 1 basename)
 adb -d pull /data/app/$XRFAndroidApkFileName
 adb -d pull /data/app/$XRFHomeApkFileName
+
+echo 'Pulling Emulator Support Files...'
 adb -d pull /sdcard/sciaps
 adb -d pull /data/data/com.sciaps.xrf/shared_prefs
 adb -d pull /storage/extsdcard/db
 adb -d pull /storage/extsdcard/ecal.log
 adb -d pull /storage/extsdcard/pin.cfg
+
+echo "Installing XRFAndroid..."
+adb -e push $XRFAndroidApkFileName /data/local/tmp/app.apk
+adb -e shell chmod 777 "/data/local/tmp/app.apk"
+adb -e shell pm install -d -r "/data/local/tmp/app.apk"
+
+echo "Installing XRFHome..."
+adb -e push $XRFHomeApkFileName /data/local/tmp/app.apk
+adb -e shell chmod 777 "/data/local/tmp/app.apk"
+adb -e shell pm install -d -r "/data/local/tmp/app.apk"
 
 echo 'Installing Emulator Support Files...'
 adb -e shell rm -rf /sdcard/sciaps
@@ -33,16 +45,6 @@ adb -e shell rm -rf /data/data/com.sciaps.xrf/shared_prefs
 adb -e shell rm -rf /sdcard/emulator
 
 adb -e shell mkdir /sdcard/emulator
-
-echo "Installing XRFAndroid"
-adb -e push $XRFAndroidApkFileName /data/local/tmp/app.apk
-adb -e shell chmod 777 "/data/local/tmp/app.apk"
-adb -e shell pm install -d -r "/data/local/tmp/app.apk"
-
-echo "Installing XRFHome"
-adb -e push $XRFHomeApkFileName /data/local/tmp/app.apk
-adb -e shell chmod 777 "/data/local/tmp/app.apk"
-adb -e shell pm install -d -r "/data/local/tmp/app.apk"
 
 adb -e push sciaps /sdcard/sciaps
 adb -e push shared_prefs /data/data/com.sciaps.xrf/shared_prefs
--- scripts/windows/emulate_xrf_analyzer.bat
+++ scripts/windows/emulate_xrf_analyzer.bat
@@ -13,18 +13,30 @@ RMDIR /S %work_dir%
 mkdir %work_dir%
 pushd %work_dir%
 
-ECHO Pulling Emulator Support Files...
+ECHO Pulling Apps...
 adb -d shell ls /data/app/com.sciaps.xrf* | xargs -n 1 basename > XRFAndroidApkFileName.txt
 set /p XRFAndroidApkFileName=<XRFAndroidApkFileName.txt
 adb -d shell ls /data/app/com.sciaps.android.home* | xargs -n 1 basename > XRFHomeApkFileName.txt
 set /p XRFHomeApkFileName=<XRFHomeApkFileName.txt
 adb -d pull /data/app/%XRFAndroidApkFileName%
 adb -d pull /data/app/%XRFHomeApkFileName%
+
+ECHO Pulling Emulator Support Files...
 adb -d pull /sdcard/sciaps
 adb -d pull /data/data/com.sciaps.xrf/shared_prefs
 adb -d pull /storage/extsdcard/db
 adb -d pull /storage/extsdcard/ecal.log
 adb -d pull /storage/extsdcard/pin.cfg
+
+ECHO Installing XRFAndroid...
+adb -e push %XRFAndroidApkFileName% /data/local/tmp/app.apk
+adb -e shell chmod 777 "/data/local/tmp/app.apk"
+adb -e shell pm install -d -r "/data/local/tmp/app.apk"
+
+ECHO Installing XRFHome...
+adb -e push %XRFHomeApkFileName% /data/local/tmp/app.apk
+adb -e shell chmod 777 "/data/local/tmp/app.apk"
+adb -e shell pm install -d -r "/data/local/tmp/app.apk"
 
 ECHO Installing Emulator Support Files...
 adb -e shell rm -rf /sdcard/sciaps
@@ -32,16 +44,6 @@ adb -e shell rm -rf /data/data/com.sciaps.xrf/shared_prefs
 adb -e shell rm -rf /sdcard/emulator
 
 adb -e shell mkdir /sdcard/emulator
-
-ECHO Installing XRFAndroid
-adb -e push %XRFAndroidApkFileName% /data/local/tmp/app.apk
-adb -e shell chmod 777 "/data/local/tmp/app.apk"
-adb -e shell pm install -d -r "/data/local/tmp/app.apk"
-
-ECHO Installing XRFHome
-adb -e push %XRFHomeApkFileName% /data/local/tmp/app.apk
-adb -e shell chmod 777 "/data/local/tmp/app.apk"
-adb -e shell pm install -d -r "/data/local/tmp/app.apk"
 
 adb -e push sciaps /sdcard/sciaps
 adb -e push shared_prefs /data/data/com.sciaps.xrf/shared_prefs
